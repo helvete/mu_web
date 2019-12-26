5cc50a46a17e09ddee65f060f97e13878bcd138e
Secondary Worker <do@not-even-try.it>
2019-12-26 14:23:50 +0100

    Drop old web iface reference

--- _oldweb_reference/html/footer.php
+++
@@ -1,3 +0,0 @@
-
-  <div id="footer">Mu Storm 97d server's web by Jimmmy &copy; time consumed: <?php $time += microtime(true); $time = round($time, 3); echo $time; ?>s memory used: <?php echo memory_get_usage(true)/1024 ?> kB<br />
-  
